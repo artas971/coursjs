9a01d68cdb1f19acef690c2fa5b4309e7a992393
steve <artas971@gmail.com>
2023-04-18 16:29:08 +0200

    exo fait

--- exo/main.js
+++ exo/main.js
@@ -1,12 +1,20 @@
 let nombres = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
-console.log(nombres.pop());
-console.log(nombres.shift());
-console.log(nombres.length);
+
+console.log(nombres.pop()); 
+console.log(nombres.shift()); 
+
+console.log(nombres.length); 
+
 for(nombre of nombres){
     console.log(nombre);
 }
+
 let prenoms = ['Ali', 'Nour', 'Jean', 'Carole', 'Ibrahim', 'Alma'];
+
 for(prenom of prenoms){
-    console.log(prenom.slice(2,6));
+    console.log(prenoms.slice(2,6));
 }
-for(prenom of prenoms){console.log("Bonjour " + prenom)}
+
+for(prenom of prenoms){
+    console.log("Bonjour " + prenom);
+}
